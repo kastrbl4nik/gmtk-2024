7398703688c893d67710e5cef39447c3354799e5
kastrbl4nik <kastrbl4nik@gmail.com>
2024-08-17 14:57:18 +0200

    feat: support multiple altars

--- Assets/Dev/Scripts/Altar.cs
+++ Assets/Dev/Scripts/Altar.cs
@@ -2,27 +2,76 @@ using System.Collections;
 using UnityEngine;
 using Cinemachine;
 using UnityEditor;
+using System.Linq;
+using UnityEngine.Rendering.Universal;
 
 public class Altar : MonoBehaviour
 {
     [SerializeField] private float pauseBeforeSpawn = 1f;
+    [SerializeField] private bool isActive;
     private GameObject playerPrefab;
     private CinemachineVirtualCamera cam;
+    private Coroutine spawnCoroutine;
+    private GameObject altarLight;
 
     private void Awake()
     {
+        altarLight = GetComponentInChildren<Light2D>().gameObject;
         playerPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Level/Prefabs/Player.prefab");
         cam = FindObjectOfType<CinemachineVirtualCamera>();
+        altarLight.SetActive(isActive);
     }
 
     private void Start()
     {
-        Spawn();
-        StartCoroutine(SpawnEntityRoutine());
+        if (isActive)
+        {
+            spawnCoroutine = StartCoroutine(SpawnCoroutine());
+        }
     }
 
-    private IEnumerator SpawnEntityRoutine()
+    public void Activate()
     {
+        DeactivateOtherAltars();
+
+        isActive = true;
+        altarLight.SetActive(true);
+
+        // kill the player who activated the altar
+        var playerToDie = FindObjectsOfType<Player>().FirstOrDefault(p => p.IsAlive);
+        if (playerToDie != null)
+        {
+            // destroy instead of die to avoid cramming the altar with dead players
+            Destroy(playerToDie.gameObject);
+        }
+
+        spawnCoroutine = StartCoroutine(SpawnCoroutine());
+    }
+
+    public void Deactivate()
+    {
+        isActive = false;
+        altarLight.SetActive(false);
+
+        StopCoroutine(spawnCoroutine);
+    }
+
+    private void DeactivateOtherAltars()
+    {
+        var altars = FindObjectsOfType<Altar>();
+
+        foreach (var altar in altars)
+        {
+            if (altar != this)
+            {
+                altar.Deactivate();
+            }
+        }
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        Spawn();
         while (true)
         {
             yield return new WaitForSeconds(Player.Lifespan + pauseBeforeSpawn);
@@ -35,5 +84,13 @@ public class Altar : MonoBehaviour
         var instanciatedEntity = Instantiate(playerPrefab, transform.position, Quaternion.identity);
         instanciatedEntity.GetComponent<Player>().OnDeath += () => cam.Follow = transform;
         cam.Follow = instanciatedEntity.transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isActive && other.CompareTag("Player"))
+        {
+            Activate();
+        }
     }
 }
