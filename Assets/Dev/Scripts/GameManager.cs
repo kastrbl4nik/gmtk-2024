58d68b18349104b29a8637390e7395e33aa4f6c0
kastrbl4nik <kastrbl4nik@gmail.com>
2024-08-17 12:42:32 +0200

    feat: setup main menu scene; add GameManager

---
+++ Assets/Dev/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
