73ef4cad3194f8d8b68bb2c4244c27d29c98ed70
kastrbl4nik <kastrbl4nik@gmail.com>
2024-08-19 11:34:23 +0200

    feat: add parallax effect on lvl 1

---
+++ Assets/Dev/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxEffectMultiplier = 0.5f;
+
+    private Transform mainCameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+        lastCameraPosition = mainCameraTransform.position;
+    }
+
+    private void Update()
+    {
+        var deltaMovement = mainCameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier, deltaMovement.y * parallaxEffectMultiplier, 0f);
+        lastCameraPosition = mainCameraTransform.position;
+    }
+}
