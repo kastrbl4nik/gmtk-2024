ff0fdf8362add61b1bcd1963b4fe1c6953df0dce
kastrbl4nik <kastrbl4nik@gmail.com>
2024-08-19 21:32:00 +0200

    feat: update main menu

--- Assets/Dev/Scripts/Player.cs
+++ Assets/Dev/Scripts/Player.cs
@@ -191,7 +191,7 @@ public class Player : MonoBehaviour, IWeightable, IKeyable
         GetComponent<PlayerController>().enabled = false;
         GetComponent<PlayerAnimator>().enabled = false;
         GetComponent<Rigidbody2D>().drag = 1000;
-        GetComponent<Rigidbody2D>().gravityScale = 3000;
+        GetComponent<Rigidbody2D>().gravityScale = 1000;
 
         OnDeath?.Invoke();
     }
