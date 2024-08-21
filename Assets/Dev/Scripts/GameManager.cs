fc831b209b31f78a6b54bdc27e086df3b651fae6
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-08-21 09:14:58 +0300

    feat: make game manager unity-specific singleton

--- Assets/Dev/Scripts/GameManager.cs
+++ Assets/Dev/Scripts/GameManager.cs
@@ -4,41 +4,59 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    private static GameManager Instance;
     private Animator animator;
     private readonly float transitionTime = 1f;
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
 
         if (Instance == null)
         {
+            DontDestroyOnLoad(gameObject);
             Instance = this;
         }
         else
         {
-            Destroy(gameObject);
             return;
         }
         animator = GetComponentInChildren<Animator>();
     }
     public void LoadScene(int index)
     {
+        if (Instance != this)
+        {
+            Instance.LoadScene(index);
+            return;
+        }
         StartCoroutine(LoadSceneWithTransition(index));
     }
 
     public void LoadNextScene()
     {
+        if (Instance != this)
+        {
+            Instance.LoadNextScene();
+            return;
+        }
         LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ReloadScene()
     {
+        if (Instance != this)
+        {
+            Instance.ReloadScene();
+            return;
+        }
         LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private IEnumerator LoadSceneWithTransition(int index)
     {
+        if (Instance != this)
+        {
+            yield return Instance.LoadSceneWithTransition(index);
+        }
         animator.SetTrigger("End");
         yield return new WaitForSecondsRealtime(transitionTime);
         var asyncLoad = SceneManager.LoadSceneAsync(index);
